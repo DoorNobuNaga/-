850d2f665ae6c67392562a70e1106336e31b39e9
DoorNobuNaga <door.nobunaga@gmail.com>
2025-01-08 00:32:20 +0900

    Update onload.js

--- src/onload.js
+++ src/onload.js
@@ -11,8 +11,6 @@ function initialize(){
             audiosVoice[i] = new Audio('./voice/' + strsFileName[i]);
             var eleBtn = document.createElement('button');
 
-            console.log(audiosVoice[i]);
-
             document.body.appendChild(eleBtn);
             eleBtn.textContent = strsFileName[i];
 
